d693bb200b87d3dcb555a7835b9649c81a85febf
Chimirinzin99 <02250346.cst@rub.edu.bt>
2026-05-10 21:59:32 +0600

    A_4

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 from flask import Flask
-app = Flask(__name__)
+import os
+
+app = Flask(_name_)
 
 @app.route('/')
 def home():
     return "Hello DevOps!"
 
-if __name__ == '__main__':
-    app.run()
+if _name_ == '_main_':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
